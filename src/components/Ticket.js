4e318e966514ca278fe5b042faa8977431d4150a
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-02 20:49:08 +0200

    display and format all available data

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import TicketSegment from './TicketSegment'
+import lib from './../util/lib';
+
 class Ticket extends Component {
   constructor(props) {
   super(props);
@@ -10,6 +12,10 @@ class Ticket extends Component {
 
   toggleHover = () => {
     this.setState({hover: !this.state.hover})
+  }
+
+  openDeeplink = (link) => {
+    window.open(link)
   }
 
   render() {
@@ -24,27 +30,29 @@ class Ticket extends Component {
           <div className="c-flights_ticket-container">
               <div className="c-flights_ticket-content">
                   <div className="c-flights_ticket-summary">
-                      <TicketSegment />
+                      <TicketSegment legs={this.props.ticketData.Leg}/>
                       <div className="c-flights_ticket-summary-deal">
                           <div className="c-flights_ticket-summary-deal-main">
                               <div className="c-flights_ticket-summary-deal-prices">
                                   <div className="c-flights_ticket-summary-deal-price c-flights_ticket-summary-deal-price--single">
-                                    333,333 HUF
+                                    {lib.convertPrice(this.props.ticketData.Offer.Price)}&nbsp;
+                                    {this.props.ticketData.Offer.Currency}
                                   </div>
                               </div>
                           </div>
                           <div className="c-flights_ticket-summary-deal-footer">
-                              <div className="c-flights_ticket-summary-deal-button c-button c-push_button">
+                              <div
+                                className="c-flights_ticket-summary-deal-button c-button c-push_button"
+                                onClick={() => this.openDeeplink(this.props.ticketData.Offer.Deeplink)}>
                                   <div className="c-button-layers">
                                       <div className="c-button-outline"></div>
                                       <div className="c-button-fill" style={linkStyle}></div>
                                   </div>
-
                                   <div
                                     className="c-button-content"
                                     style={{color: "white"}}
-                                    onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}
-                                    >
+                                    onMouseEnter={this.toggleHover}
+                                    onMouseLeave={this.toggleHover}>
                                       Book
                                   </div>
                               </div>
