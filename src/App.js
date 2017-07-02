d9157b66d46c9e46725141d0bd033788d48d0f39
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-02 19:15:47 +0200

    add header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="c-app">
           <div className="c-app_developertest">
+              <Header />
               <div className="c-app_developertest-body">
                   <div className="c-app_developertest-layout">
                       <div className="c-app_developertest-body-content">
