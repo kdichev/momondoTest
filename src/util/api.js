40605551f99c335746aa1abbe0264dbea5e38fab
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-03 10:20:04 +0200

    gather all delta data

--- src/util/api.js
+++ src/util/api.js
@@ -1,17 +1,23 @@
 
 const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 const TARGET_URL = 'http://momondodevapi.herokuapp.com/api/1.0/FlightSearch/'
-let data;
+let data = {
+  Flights: [],
+  Legs: [],
+  Offers: [],
+  Segments: []
+};
 
 exports.fetch = (UDID, callback) => {
   fetch(PROXY_URL + TARGET_URL + UDID)
     .then(blob => blob.json())
     .then(response => {
-      if (response.ResultNumber === 0) {
-        data = response;
-      }
+      console.log(data);
+      data.Flights = data.Flights.concat(response.Flights)
+      data.Legs = data.Legs.concat(response.Legs)
+      data.Offers = data.Offers.concat(response.Offers)
+      data.Segments = data.Segments.concat(response.Segments)
       if (response.Done === true) {
-        data.Done = response.Done
         callback(data);
       } else {
         this.fetch(UDID, callback)
