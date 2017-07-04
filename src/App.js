df692d1844c476dc7d2e96fb019e6ef2a015942b
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-04 15:25:13 +0200

    refacture getOfferAndFlights, prepareRenderData, concatinateData into App because they controll the application

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import api from './util/api.js';
 import lib from './util/lib';
 import _ from 'lodash'
 
+let data = {
+  Flights: [],
+  Legs: [],
+  Offers: [],
+  Segments: []
+};
+
 class App extends Component {
   constructor(props) {
   super(props);
@@ -15,13 +22,56 @@ class App extends Component {
     };
   }
 
+  getOfferAndFlights = (data, callback) => {
+    data.Offers.map((offer, index) => {
+      if (typeof data.Flights[offer.FlightIndex] !== 'undefined') {
+        var a = data.Flights[offer.FlightIndex].Key.split('|');
+        var b = a[1].split(',');
+        data.Flights[offer.FlightIndex].FlightKeys = b
+        let foundLegs = []
+        data.Flights[offer.FlightIndex].SegmentIndexes.map((segmentId) => {
+          data.Segments[segmentId].LegIndexes.map((leg, index) => {
+            if (leg === segmentId) {
+              foundLegs.push(data.Legs[leg]);
+            }
+          })
+          callback(offer, data.Flights[offer.FlightIndex], data.Segments[segmentId], foundLegs);
+        })
+      }
+    })
+  }
+
+  prepareRenderData = (data, callback) => {
+    let finalData = [];
+    this.getOfferAndFlights(data, (offer, flight, segment, leg) => {
+      finalData.push({
+        'Offer': offer,
+        'Flight': flight,
+        'Segment': segment,
+        'Leg': leg
+      });
+    });
+    finalData.sort((a, b) => {
+      return a.Offer.Price - b.Offer.Price
+    });
+    callback(finalData);
+  }
+
+  concatinateData = (response) => {
+    data.Flights = data.Flights.concat(response.Flights)
+    data.Legs = data.Legs.concat(response.Legs)
+    data.Offers = data.Offers.concat(response.Offers)
+    data.Segments = data.Segments.concat(response.Segments)
+    return data
+  }
+
   componentDidMount() {
     const UDID = lib.getUDID();
     api.fetch(UDID, (response, err) => {
       if (err) {
         console.log("Error loading data: ", err);
       } else {
-        lib.prepareRenderData(lib.concatinateData(response), (data) => {
+        this.prepareRenderData(this.concatinateData(response), (data) => {
           var uniq = _.uniqBy(data, (o) => {
             return o.Flight.Key;
           });
--- src/util/lib.js
+++ src/util/lib.js
@@ -7,41 +7,6 @@ function s4() {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
 
-exports.getOfferAndFlights = (data, callback) => {
-  data.Offers.map((offer, index) => {
-    if (typeof data.Flights[offer.FlightIndex] !== 'undefined') {
-      var a = data.Flights[offer.FlightIndex].Key.split('|');
-      var b = a[1].split(',');
-      data.Flights[offer.FlightIndex].FlightKeys = b
-      let foundLegs = []
-      data.Flights[offer.FlightIndex].SegmentIndexes.map((segmentId) => {
-        data.Segments[segmentId].LegIndexes.map((leg, index) => {
-          if (leg === segmentId) {
-            foundLegs.push(data.Legs[leg]);
-          }
-        })
-        callback(offer, data.Flights[offer.FlightIndex], data.Segments[segmentId], foundLegs);
-      })
-    }
-  })
-}
-
-exports.prepareRenderData = (data, callback) => {
-  let finalData = [];
-  this.getOfferAndFlights(data, (offer, flight, segment, leg) => {
-    finalData.push({
-      'Offer': offer,
-      'Flight': flight,
-      'Segment': segment,
-      'Leg': leg
-    });
-  });
-  finalData.sort((a, b) => {
-    return a.Offer.Price - b.Offer.Price
-  });
-  callback(finalData);
-}
-
 exports.convertPrice = (number) => {
   var a = number.toString().slice(0,6);
   var b = a.slice(0,3);
@@ -59,19 +24,4 @@ exports.convertMinsToHrsMins = (minutes) => {
 
   }
   return h + 'h ' + m + 'm';
-}
-
-let data = {
-  Flights: [],
-  Legs: [],
-  Offers: [],
-  Segments: []
-};
-
-exports.concatinateData = (response) => {
-  data.Flights = data.Flights.concat(response.Flights)
-  data.Legs = data.Legs.concat(response.Legs)
-  data.Offers = data.Offers.concat(response.Offers)
-  data.Segments = data.Segments.concat(response.Segments)
-  return data
 }
