63ab118a1003e848f625524dbef5cf38f231f04d
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-05 14:19:15 +0200

    comment and clean up Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+/**
+* Loader - displays loader
+* @prop  {bool} show
+*/
 class Loader extends Component {
   render() {
     return (
       <div style={{width: "100%", textAlign: "center"}}>
         {this.props.show &&
-          <img src="https://cdn.momondo.net/assets/temporary/travel-saver-calendar/wheel.gif" width="30px" height="30px"/>
+          <img
+            src="https://cdn.momondo.net/assets/temporary/travel-saver-calendar/wheel.gif"
+            width="30px"
+            height="30px"
+            alt="presentation"
+          />
         }
       </div>
     );
